c465f37f03f7d36031f80dbdca48c22843f66a3e
Jess ward <JKRWard@github.com>
2019-02-22 11:09:53 +0000

    setting up files

---
+++ params/params_jw.R
@@ -0,0 +1,13 @@
+###### JW parameters
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <-  0.2
+
+# e.g. "jessica_wardinatus"    
+species.name <- "jessica_wardinatus"
+
+# e.g. "turquoise"    
+color <- "turquoise4"
+
+# tip: pick a color at using rstudio colour picker. Addins > Colour Picker
