aabda8264382de83adfed6d1affe57cd94eff9a0
m-louise <louise.mair@newcastle.ac.uk>
2019-02-25 14:28:26 +0000

    This commit creates this file.

---
+++ params/params_LM.R
@@ -0,0 +1,14 @@
+# This is my edit
+# ________________________________________________
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <-  1.5
+
+# e.g. "jessica_wardinatus"    
+species.name <- "louise_equus"
+
+# e.g. "turquoise"    
+color <- "purple"
+
+# tip: pick a color at using rstudio colour picker. Addins > Colour Picker
