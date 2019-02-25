6d0d93035c9b7a02eee606c857f201f1ad1a8726
TurnipEagle <B.W.Rowland1@newcastle.ac.uk>
2019-02-25 14:56:01 +0000

    change colour and sig

--- params_ben.R
+++ params_ben.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-  3.0
+sig2 <-  3.3
 
 # e.g. "jessica_wardinatus"    
 species.name <- "Turnip_Eagle"
 
 # e.g. "turquoise"    
-color <- "green2"
+color <- "green3"
 
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
