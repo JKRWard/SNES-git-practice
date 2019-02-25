9c8cc780a5dbebc902a3b04139636fa8fd666788
m-louise <louise.mair@newcastle.ac.uk>
2019-02-25 14:57:16 +0000

    create another file

---
+++ params/params_LM2.R
@@ -0,0 +1,14 @@
+# This is a second version
+# ________________________________________________
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <-  4.5
+
+# e.g. "jessica_wardinatus"    
+species.name <- "dressius_maximus"
+
+# e.g. "turquoise"    
+color <- "magenta"
+
+# tip: pick a color at using rstudio colour picker. Addins > Colour Picker
